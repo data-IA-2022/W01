0d112cb4427fd0941a5dd22c6af475158628c9c9
Emmanuel Goudot <Emmanuel.Goudot@gmail.com>
2023-01-04 13:54:39 +0100

    Modif index

--- prog_w01.py
+++ prog_w01.py
@@ -5,7 +5,14 @@ print(f"parametres : {sys.argv[1:]}")
 
 #fns = glob.glob('fao_2013/*.csv')
 #fns = glob.glob('**/*.csv', recursive=True)
+names = ['Code Domaine', 'Domaine', 'Code Pays', 'Pays', 'Code Élément',
+         'Élément', 'Code Produit', 'Produit', 'Code Année', 'Année', 'Unité',
+         'Valeur', 'Symbole', 'Description du Symbole']
 
 for fn in sys.argv[1:]:
-    df = pd.read_csv(fn)
+    df = pd.read_csv(fn, names=names, header=0)
+    df.index = df['Code Pays']
     print(f"- {fn:50} {df.shape}")
+    print(df.columns)
+    print(df)
+
