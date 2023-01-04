b2313060006800ebf47a889cb258a03bfda7f1fb
Emmanuel Goudot <Emmanuel.Goudot@gmail.com>
2023-01-04 11:19:30 +0100

    Ajout exe shell avec appel python avec parametres

---
+++ appli.sh
@@ -0,0 +1,4 @@
+echo 'Liste des fichiers du répertoire:'
+#echo "parametres: $1, $2, $3, ou $@"
+ls
+python3.8 prog_w01.py **/*.csv
--- prog_w01.py
+++ prog_w01.py
@@ -1,9 +1,11 @@
-import glob
+import glob, sys
 import pandas as pd
 
-#fns = glob.glob('fao_2013/*.csv')
-fns = glob.glob('**/*.csv', recursive=True)
+print(f"parametres : {sys.argv[1:]}")
 
-for fn in fns:
+#fns = glob.glob('fao_2013/*.csv')
+#fns = glob.glob('**/*.csv', recursive=True)
+
+for fn in sys.argv[1:]:
     df = pd.read_csv(fn)
     print(f"- {fn:50} {df.shape}")
