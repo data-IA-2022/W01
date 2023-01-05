144d81b22dab87d0d5a32fea6ca2dc0652f138bf
Emmanuel Goudot <Emmanuel.Goudot@gmail.com>
2023-01-05 16:17:22 +0100

    Requete SQL join

--- sql_use.py
+++ sql_use.py
@@ -6,5 +6,20 @@ from colorama import Fore, Back, Style
 
 conn = sqlite3.connect('bdd.sqlite')
 
-df = pd.read_sql_query("""select "Code Pays",Pays,Valeur from population;""", conn)
+df = pd.read_sql("""select "Code Pays",Pays,Valeur  AS Valeur_population from population;""", conn)
+#print(df)
+
+df = pd.read_sql("""select "Code Pays",Pays,Valeur AS Valeur_cereal from cereal WHERE Élément='Production' AND Produit="Blé" ORDER BY "Code Pays";""", conn)
+#print(df)
+
+SQL = '''
+SELECT population."Code Pays", population."Pays", population.Valeur AS population, cereal.Valeur AS cereal
+FROM population INNER JOIN cereal
+ON (population."Code Pays" = cereal."Code Pays")
+WHERE (cereal.Élément='Production' AND cereal.Produit="Blé")
+ORDER BY population."Code Pays"
+; 
+'''
+
+df = pd.read_sql(SQL, conn)
 print(df)
