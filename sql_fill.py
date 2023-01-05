713aff720419f903b1640b23d6456627e6dfa891
Emmanuel Goudot <Emmanuel.Goudot@gmail.com>
2023-01-05 15:14:22 +0100

    Ajout creation BDD

---
+++ sql_fill.py
@@ -0,0 +1,33 @@
+import glob, sys, sqlite3
+import pandas as pd
+import numpy as np
+from colorama import Fore, Back, Style
+
+# Ouverture sqllite
+conn = sqlite3.connect('bdd.sqlite')
+
+# Noms de colonnes à utiliser
+names = ['Code Domaine', 'Domaine', 'Code Pays', 'Pays', 'Code Élément',
+         'Élément', 'Code Produit', 'Produit', 'Code Année', 'Année', 'Unité',
+         'Valeur', 'Symbole', 'Description du Symbole']
+
+# Récupération de la liste de CSV si fournie
+# Ou utilisation de glob
+fns = sys.argv[1:]
+if not fns:
+    fns = glob.glob('*/*.csv') # 'fao_2013/*.csv'
+print(f"parametres : {fns}")
+
+#fns = glob.glob('fao_2013/*.csv')
+#fns = glob.glob('**/*.csv', recursive=True)
+
+# Lecture puis affichage des info taille des CSV
+for fn in fns:
+    fns = fn[22:-4]
+    df = pd.read_csv(fn, names=names, header=0)
+    #df.index = df['Code Pays']
+    print(f"- {fn:50s} ({df.shape[0]:6d}, {df.shape[1]:2d}) - {Fore.BLACK}{Back.GREEN}{fns}{Style.RESET_ALL}")
+    df.to_sql(fns, con=conn, if_exists='replace')
+    #print(df.columns)
+    #print(df)
+
