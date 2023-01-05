aa9a87aff3a0545a2daf66f3220d737e56644f1c
Emmanuel Goudot <Emmanuel.Goudot@gmail.com>
2023-01-05 16:56:56 +0100

    SQL->scatter

--- sql_use.py
+++ sql_use.py
@@ -6,10 +6,10 @@ from colorama import Fore, Back, Style
 
 conn = sqlite3.connect('bdd.sqlite')
 
-df = pd.read_sql("""select "Code Pays",Pays,Valeur  AS Valeur_population from population;""", conn)
+#df = pd.read_sql("""select "Code Pays",Pays,Valeur  AS Valeur_population from population;""", conn)
 #print(df)
 
-df = pd.read_sql("""select "Code Pays",Pays,Valeur AS Valeur_cereal from cereal WHERE Élément='Production' AND Produit="Blé" ORDER BY "Code Pays";""", conn)
+#df = pd.read_sql("""select "Code Pays",Pays,Valeur AS Valeur_cereal from cereal WHERE Élément='Production' AND Produit="Blé" ORDER BY "Code Pays";""", conn)
 #print(df)
 
 SQL = '''
@@ -17,9 +17,24 @@ SELECT population."Code Pays", population."Pays", population.Valeur AS populatio
 FROM population INNER JOIN cereal
 ON (population."Code Pays" = cereal."Code Pays")
 WHERE (cereal.Élément='Production' AND cereal.Produit="Blé")
+    AND cereal.Valeur>0
 ORDER BY population."Code Pays"
 ; 
 '''
 
 df = pd.read_sql(SQL, conn)
 print(df)
+
+x = np.log(df['population'])
+y = np.log(df['cereal'])
+
+plt.scatter(x, y) # le graph
+
+# Ajout des labels pour chaque ligne (coordonnées en LOG)
+for i,x in df.iterrows():
+    plt.annotate(x['Pays'],
+                 (np.log(x['population']), np.log(x['cereal'])),
+                 fontsize=7)
+
+# Affichage
+plt.show()
