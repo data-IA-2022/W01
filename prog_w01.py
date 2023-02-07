eaefe26bb7dfb6d212ef98ccafc5713c891126ea
Emmanuel Goudot <Emmanuel.Goudot@gmail.com>
2023-02-07 08:53:57 +0100

    Suppression ancien fichiers

--- prog_w01.py
+++
@@ -1,9 +0,0 @@
-import glob
-import pandas as pd
-
-#fns = glob.glob('fao_2013/*.csv')
-fns = glob.glob('**/*.csv', recursive=True)
-
-for fn in fns:
-    df = pd.read_csv(fn)
-    print(f"- {fn:50} {df.shape}")
